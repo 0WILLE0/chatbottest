df63a0ab2577521fd98b2e942928bc8d9850e0fa
0WILLE0 <wille5852@gmail.com>
2018-06-29 16:03:38 +0900

    키보드 추가

--- firsttest/home/views.py
+++ firsttest/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+def keyboard(request):
+    return JsonResponse
 # Create your views here.
