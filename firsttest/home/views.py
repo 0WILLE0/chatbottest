8e6270e67d7e1b3db90f3f1245905884374cdce9
0WILLE0 <wille5852@gmail.com>
2018-06-29 17:01:18 +0900

    응답 메세지 수정

--- firsttest/home/views.py
+++ firsttest/home/views.py
@@ -2,9 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from bs4 import BeautifulSoup
+import urllib.request
+import json
+import datetime
+import requests
 
-def keyboard(request):
-    return JsonResponse({"type" : "buttons", "buttons" : ["선택 1", "선택 2", "선택 3"]})
-def message(request):
-    return
-# Create your views here.
+def keyboard (request) :
+    return JsonResponse(
+        {
+            "type" : "buttons",
+            "buttons" : ["인사", "", "선택 3"],
+        }
+    )
+
+@csrf_exempt
+def message(request) :
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content']  #버튼 항목중 무엇을 눌렀는가
+    
+    if return_str == '인사' :
+        return JsonResponse({
+           "message" : {
+               "text" : "안녕하세요" 
+               }
+        })
+    if return_str == '인사' :
+        return JsonResponse({
+           "message" : {
+               "text" : "안녕하세요" 
+               }
+        })
+    if return_str == '인사' :
+        return JsonResponse({
+           "message" : {
+               "text" : "안녕하세요" 
+               }
+        })
