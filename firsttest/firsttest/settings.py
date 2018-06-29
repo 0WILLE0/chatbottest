fbc248f5f04b0c45fffb617525766ba220f66100
0WILLE0 <wille5852@gmail.com>
2018-06-29 15:34:21 +0900

    모든 ip 허용

--- firsttest/firsttest/settings.py
+++ firsttest/firsttest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u@d1(p!qbcc@u31lq(tw501s7cn2&-uj7y9x^d$rt#tu(be41*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home', 
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
